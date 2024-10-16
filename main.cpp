2b33ec3f5c66464e2947fc460f3cf16a98987647
AttilaBalogh14 <battis040414@gmail.com>
2024-10-16 12:09:50 +0200

    Fellelt hibák kommentezése

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; // Név elírás N_ELEMENTS
+    std::cout << '1-100 ertekek duplazasa' // karakterhez használatos idézőjel hiba + hiányzó pontosvessző
+    for (int i = 0;)  //for ciklusban hiány 
     {
         b[i] = i * 2;
     }
     for (int i = 0; i; i++)
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" 
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS, i++) // for cikluson belül pontosvessző helyett sima vessző
     {
-        atlag += b[i]
+        atlag += b[i]  //pontosvessző hiány
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
