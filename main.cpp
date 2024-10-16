d4f39b2eefc438f1e2e61e21df9892c125838c4c
AttilaBalogh14 <battis040414@gmail.com>
2024-10-16 12:33:18 +0200

    Hibák kijavítása

--- main.cpp
+++ main.cpp
@@ -4,23 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; // Név elírás N_ELEMENTS
-    std::cout << '1-100 ertekek duplazasa' // karakterhez használatos idézőjel hiba + hiányzó pontosvessző
-    for (int i = 0;)  //for ciklusban hiány 
+    int *b = new int[N_ELEMENTS]; // Név elírás N_ELEMENTS
+    std::cout << "1-100 ertekek duplazasa" << std::endl; // karakterhez használatos idézőjel hiba + hiányzó pontosvessző
+    for (int i = 1;i<=N_ELEMENTS;i++)  //for ciklusban hiány 
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 1; i<= N_ELEMENTS; i++)
     {
-        std::cout << "Ertek:" 
+        std::cout << "Ertek: " << b[i] << std::endl;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++) // for cikluson belül pontosvessző helyett sima vessző
+    int atlag = 0;
+    int osszeg = 0;
+    for (int i = 1; i <= N_ELEMENTS; i++) // for cikluson belül pontosvessző helyett sima vessző
     {
-        atlag += b[i]  //pontosvessző hiány
+        osszeg += b[i];  //pontosvessző hiány
     }
-    atlag /= N_ELEMENTS;
+    atlag = osszeg / N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
     return 0;
 }
